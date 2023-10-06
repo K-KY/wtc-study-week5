feb6706701761397964f506c29729da45532aa4a
K-KY <kyuyoungk@naver.com>
2023-10-06 15:45:26 +0900

    Fix : MenuCategory에서 enum리스트를 리턴하도록 변경

--- src/main/java/menu/enums/MenuCategory.java
+++ src/main/java/menu/enums/MenuCategory.java
@@ -1,15 +1,21 @@
 package menu.enums;
+
+import java.util.Arrays;
+import java.util.List;
+
 //1이면 일식, 2면 한식, 3이면 중식, 4면 아시안, 5면 양식을 추천해야 한다.
 public enum MenuCategory {
-    일식("일식"), 한식("한식"), 중식("중식"), 아시안("아시안"), 양식("양식");
-
-    private final String category;
-
-    MenuCategory(String category) {
+    일식(Japan.values()),
+    한식(Korea.values()),
+    중식(China.values()),
+    아시안(Asian.values()),
+    양식(Northern.values());
+    private final Enum[] category;
+    MenuCategory(Enum[] category) {
         this.category = category;
     }
 
-    public String getCategory() {
-        return category;
+    public List<Enum> getCategory() {
+        return Arrays.asList(category);
     }
 }
