4e54701c6aef8b20f9e38646e3962a61436a74f8
K-KY <kyuyoungk@naver.com>
2023-10-06 14:29:45 +0900

    Feat : 메뉴별 enum

---
+++ src/main/java/menu/enums/Asian.java
@@ -0,0 +1,16 @@
+package menu.enums;
+
+public enum Asian {
+    팟타이("팟타이"), 카오팟("카오 팟"), 나시고렝("나시고랭"), 파인애플볶음밥("파인애플 볶음밥"), 쌀국수("쌀국수"), 똠얌꿍("똠얌꿍"), 반미("반미"), 월남쌈("월남쌈"), 분짜("분짜");
+    private final String menu;
+
+    Asian(String menu) {
+        this.menu = menu;
+    }
+    public String getMenu() {
+        return menu;
+    }
+
+
+
+}
