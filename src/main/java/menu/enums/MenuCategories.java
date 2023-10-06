5dba47f88b134e016977dcbd53641757f0fe9bef
K-KY <kyuyoungk@naver.com>
2023-10-06 23:38:42 +0900

    Fix : 경고메세지 안뜨게 바꿈

--- src/main/java/menu/enums/MenuCategories.java
+++ src/main/java/menu/enums/MenuCategories.java
@@ -4,18 +4,20 @@ import java.util.Arrays;
 import java.util.List;
 
 //1이면 일식, 2면 한식, 3이면 중식, 4면 아시안, 5면 양식을 추천해야 한다.
-public enum MenuCategory {
+public enum MenuCategories {
     일식(Japan.values()),
     한식(Korea.values()),
     중식(China.values()),
     아시안(Asian.values()),
     양식(Northern.values());
-    private final Enum[] category;
-    MenuCategory(Enum[] category) {
+    private final Enum<?>[] category;
+    MenuCategories(Enum<?>[] category) {
         this.category = category;
     }
 
-    public List<Enum> getCategory() {
+    public List<Enum<?>> getCategory() {
         return Arrays.asList(category);
     }
+
+
 }
