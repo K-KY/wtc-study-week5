535a55eb085dc980d6f69202da52c067bb1573ce
K-KY <kyuyoungk@naver.com>
2023-10-06 16:17:35 +0900

    Feat : 시작 메세지 출력

---
+++ src/main/java/menu/View/SystemOutput.java
@@ -0,0 +1,14 @@
+package menu.View;
+
+public class SystemOutput {
+    private final String START_MESSAGE = "점심 메뉴 추천을 시작합니다.";
+    private final String INPUT_MEMBER = "코치의 이름을 입력해 주세요. (, 로 구분)";
+    private final String INPUT_MEMBERS_DISLIKE = "(이)가 못 먹는 메뉴를 입력해 주세요.";
+    private final String THIS_IS_RESULT = "메뉴 추천 결과입니다.";
+    private final String RECOMMEND_COMPLETE = "추천을 완료했습니다.";
+
+    public void printStartMessage() {
+        System.out.println(START_MESSAGE);
+    }
+
+}
