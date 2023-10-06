18957a54cc16038796756065405613d04554fc32
K-KY <kyuyoungk@naver.com>
2023-10-07 02:37:38 +0900

    Feat : 코치이름 입력 검증메소드

---
+++ src/main/java/menu/Model/validator/InputValidator.java
@@ -0,0 +1,21 @@
+package menu.Model.validator;
+
+public class InputValidator {
+    public void permittedInputValidator(String input) {
+
+        String str = input.replace(",", "");
+        if (str.matches("[a-zA-Z가-힣,\\s]*")) {
+            return;
+        }
+        throw new IllegalArgumentException("[ERROR] 허용되지 않은 문자가 포함되어 있습니다.");
+    }
+    public void nameLengthValidator(String input) {
+        String[] memberList = input.split(",");
+        for (int i = 0; i < memberList.length; i++) {
+            if (memberList[i].trim().length() > 4 || memberList[i].trim().length() < 2) {
+                throw new IllegalArgumentException("[ERROR] 코치의 이름은 최소 2글자 최대 4글자 입니다");
+            }
+        }
+
+    }
+}
