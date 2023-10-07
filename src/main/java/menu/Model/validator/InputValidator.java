e8667a28bc0d7cb263457ea5a3e55740a7e292c4
K-KY <kyuyoungk@naver.com>
2023-10-07 11:14:11 +0900

    Feat : 못먹는 메뉴에 허용되지 않은 문자가 존재하는지 확인

--- src/main/java/menu/Model/validator/InputValidator.java
+++ src/main/java/menu/Model/validator/InputValidator.java
@@ -6,7 +6,6 @@ public class InputValidator {
         nameLengthValidator(input);
     }
     private void permittedInputValidator(String input) {
-
         String str = input.replace(",", "");
         if (str.matches("[a-zA-Z가-힣,\\s]*")) {
             return;
@@ -17,10 +16,19 @@ public class InputValidator {
     private void nameLengthValidator(String input) {
         String[] memberList = input.split(",");
         for (int i = 0; i < memberList.length; i++) {
-            if (memberList[i].trim().length() > 4 || memberList[i].trim().length() < 2) {
+            String member = memberList[i].trim();
+            if (member.length() > 4 || member.length() < 2) {
                 throw new IllegalArgumentException("[ERROR] 코치의 이름은 최소 2글자 최대 4글자 입니다");
             }
         }
-
     }
+
+    public void validateMemberDislike(String dislike) {
+        String str = dislike.replace(",", "");
+        if (str.matches("[a-zA-Z가-힣,\\s]*")) {
+            return;
+        }
+        throw new IllegalArgumentException("[ERROR] 허용되지 않은 문자가 포함되어 있습니다.");
+    }
+
 }
