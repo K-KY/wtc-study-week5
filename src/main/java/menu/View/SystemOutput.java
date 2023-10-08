14226d1fe9b7dbe1da7e60c2dd0c8adac94d4f2c
K-KY <kyuyoungk@naver.com>
2023-10-08 15:40:50 +0900

    Feat : 시스템 메세지

--- src/main/java/menu/View/SystemOutput.java
+++ src/main/java/menu/View/SystemOutput.java
@@ -1,20 +1,30 @@
 package menu.View;
 
 public class SystemOutput {
-    private final String START_MESSAGE = "점심 메뉴 추천을 시작합니다.";
+    private final String START_MESSAGE = "점심 메뉴 추천을 시작합니다.\n";
     private final String INPUT_MEMBER = "코치의 이름을 입력해 주세요. (, 로 구분)";
-    private final String INPUT_MEMBERS_DISLIKE = " (이)가 못 먹는 메뉴를 입력해 주세요.";
-    private final String THIS_IS_RESULT = "메뉴 추천 결과입니다.";
-    private final String RECOMMEND_COMPLETE = "추천을 완료했습니다.";
+    private final String INPUT_MEMBERS_DISLIKE = "(이)가 못 먹는 메뉴를 입력해 주세요.";
+    private final String THIS_IS_RESULT = "메뉴 추천 결과입니다.\n";
+    private final String RECOMMEND_COMPLETE = "\n추천을 완료했습니다.";
 
     public void printStartMessage() {
         System.out.println(START_MESSAGE);
     }
+
     public void printInputMember() {
         System.out.println(INPUT_MEMBER);
     }
+
     public void printInputMemberDislike(String member) {
-        System.out.println(member + INPUT_MEMBERS_DISLIKE);
+        System.out.println("\n" + member + INPUT_MEMBERS_DISLIKE);
+    }
+
+    public void printResult() {
+        System.out.println(THIS_IS_RESULT);
+    }
+
+    public void printComplete() {
+        System.out.println(RECOMMEND_COMPLETE);
     }
 
 }
