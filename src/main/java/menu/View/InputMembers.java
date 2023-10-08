93db21adc4d381968cff92d3f9a9d0b2e8c079c7
K-KY <kyuyoungk@naver.com>
2023-10-08 15:34:32 +0900

    Fix : 처음 잘못된 값 입력시 잘못된 값을 객체로 만드는 오류 수정

--- src/main/java/menu/View/InputMembers.java
+++ src/main/java/menu/View/InputMembers.java
@@ -8,16 +8,18 @@ import java.util.ArrayList;
 
 public class InputMembers {
     SystemOutput systemOutput = new SystemOutput();
+    String validatedMembers = "";
     public ArrayList<String> inputMembers(InputValidator validator) {
         systemOutput.printInputMember();
         String members = Console.readLine();
         isCorrect(validator, members);
-        return new DataMapping().StringToList(members);
+        return new DataMapping().StringToList(validatedMembers);
     }
 
     private void isCorrect(InputValidator validator, String members) {
         try{
             validator.validateMemberName(members);
+            validatedMembers = members;
         } catch(Exception e) {
             System.out.println(e.getMessage());
             inputMembers(validator);
